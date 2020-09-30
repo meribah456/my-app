055363ec17ec6bba3bd96240882e3e9a6133cadc
meribah456 <62520749+meribah456@users.noreply.github.com>
2020-10-01 01:40:42 +0200

    Updates

---
+++ precache-manifest.49925e4c860c2a66677cca1be4bafb5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfea992340f72694841b9a65db098c44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fecb5a519b16c514814",
+    "url": "/static/css/2.b9c16c38.chunk.css"
+  },
+  {
+    "revision": "a96e47f3fe55c3239897",
+    "url": "/static/css/main.b4b27b81.chunk.css"
+  },
+  {
+    "revision": "3fecb5a519b16c514814",
+    "url": "/static/js/2.83c9330f.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/static/js/2.83c9330f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96e47f3fe55c3239897",
+    "url": "/static/js/main.ad95f66d.chunk.js"
+  },
+  {
+    "revision": "b4b0621d6cd5d2bac561",
+    "url": "/static/js/runtime-main.fde95809.js"
+  }
+]);
